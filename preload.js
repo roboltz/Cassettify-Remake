1e3cfa3ac7309df846f1307c288abc1f2c802e59
Ethan Bolton <ethanhbolton@gmail.com>
2026-02-13 16:40:11 +0000

    added ffmpeg functions

--- preload.js
+++ preload.js
@@ -1,5 +1,45 @@
 const { contextBridge, ipcRenderer } = require('electron');
+const { exec } = require('node:child_process');
+const util = require('util');
+const execPromise = util.promisify(exec);
 
-contextBridge.exposeInMainWorld('versions', {
-  ping: () => ipcRenderer.invoke('ping')
+async function runCommand(cmd) {
+  console.log("running command: " + cmd);
+  const { stdout, _ } = await execPromise(cmd);
+  if (stdout) {
+    return stdout;
+  } else {
+    return -1;
+  }
+}
+
+// Returns the title of audio
+async function audioTitle(audioPath) {
+  const cmd = `ffprobe -v error -show_entries format_tags=title -of default=nw=1:nk=1 "${audioPath}"`;
+  return await runCommand(cmd);
+};
+
+// Returns the artist of audio
+async function audioArtist(audioPath) {
+  const cmd = `ffprobe -v error -show_entries format_tags=artist -of default=nw=1:nk=1 "${audioPath}"`;
+  return await runCommand(cmd);
+};
+
+// Returns the duration of audio
+async function audioDuration(audioPath) {
+  const cmd = `ffprobe -i "${audioPath}" -show_entries format=duration -v quiet -of csv="p=0"`;
+  return await runCommand(cmd);
+};
+
+// Outputs the song cover as an image file to destination
+async function retrieveAudioCover(audioPath, outputImagePath) {
+  const cmd = `ffmpeg -i "${audioPath}" -an -c:v copy "${outputImagePath}"`;
+  return await runCommand(cmd);
+};
+
+contextBridge.exposeInMainWorld('ffmpeg', {
+  audioTitle: audioTitle,
+  audioArtist: audioArtist,
+  audioDuration: audioDuration,
+  retrieveAudioCover: retrieveAudioCover
 });
--- renderer.js
+++ renderer.js
@@ -1,6 +1,8 @@
 const func = async () => {
-  const response = await window.versions.ping()
-  console.log(response) // prints out 'pong'
+  console.log(await window.ffmpeg.audioDuration("./music/song.mp3"));
+  console.log(await window.ffmpeg.audioTitle("./music/song.mp3"));
+  console.log(await window.ffmpeg.audioArtist("./music/song.mp3"));
+  await window.ffmpeg.retrieveAudioCover("./music/song.mp3", "./cassettes/!default/cover.jpg");
 }
 
-func()
+func();
