ac387c1812d6c5d60e3ba3ff4769692538347b5d
Ethan Bolton <ethanhbolton@gmail.com>
2026-02-16 18:52:44 +0000

    made album covers get renamed to their hashes to avoid the cover from being duplicated ingame

--- js/preload/metaFinder.js
+++ js/preload/metaFinder.js
@@ -1,6 +1,7 @@
 const { exec } = require('node:child_process');
 const fs = require('node:fs/promises');
 const path = require('path');
+const crypto = require("crypto")
 const util = require('util');
 const execPromise = util.promisify(exec);
 
@@ -12,6 +13,18 @@ async function runCommand(cmd) {
   } else {
     return null;
   }
+}
+
+// Returns the hash of any inputted file
+function returnHash(filePath) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash('sha1');
+    const stream = require('fs').createReadStream(filePath);
+
+    stream.on('error', reject);
+    stream.on('data', chunk => hash.update(chunk));
+    stream.on('end', () => resolve(hash.digest('hex')));
+  });
 }
 
 // Returns the title of audio
@@ -32,25 +45,41 @@ async function audioDuration(audioPath) {
   return await runCommand(cmd)
 };
 
-// Outputs the song cover as an image file to destination
-async function retrieveAudioCover(audioPath, outputImagePath) {
-  const cmd = `ffmpeg -i "${audioPath}" -an -c:v copy "${outputImagePath}"`;
-  return await runCommand(cmd);
+
+// Outputs the song cover named after its hash to the cover images folder
+async function retrieveAudioCover(audioPath) {
+  const coverFolderPath = './cassetteAlbumCovers/';
+  const tempCover = './temp/cover.jpg';
+  fs.mkdir(coverFolderPath, { recursive: true });
+
+  const cmd = `ffmpeg -y -i "${audioPath}" -map 0:v:0 -an "${tempCover}"`;
+  await runCommand(cmd);
+
+  const coverHash = await returnHash(tempCover);
+  await fs.rename(tempCover, coverFolderPath + coverHash + ".jpg");
+  
+  return coverHash;
 };
 
-async function convertAudio(audioPath, outputAudioPath) {
-  const cmd = `ffmpeg -i "${audioPath}" "${outputAudioPath}"`;
+
+// Converts audio to another file type
+async function convertAudio(audioPath, outputPath) {
+  const cmd = `ffmpeg -i "${audioPath}" "${outputPath}"`;
   return await runCommand(cmd);
 }
 
+
 module.exports = async function initializeAudio (audioPath) {
+  await fs.mkdir('./temp/');
+
   const songUUID = crypto.randomUUID();
   const folderPath = './cassettes/' + songUUID + '/';
   
-  fs.mkdir(folderPath + "originalAudio/", { recursive: true });
+  await fs.mkdir(folderPath + "originalAudio/", { recursive: true });
 
   const audioMeta = new Object();
   audioMeta.filename = path.basename(audioPath);
+  audioMeta.coverHash = await retrieveAudioCover(audioPath);
   audioMeta.title = await audioTitle(audioPath);
   audioMeta.artist = await audioArtist(audioPath);
   audioMeta.duration = await audioDuration(audioPath);
@@ -58,7 +87,8 @@ module.exports = async function initializeAudio (audioPath) {
   const audioMetaStr = JSON.stringify(audioMeta, null, 2).replace(/\\n/g, '')
   await fs.writeFile(folderPath + "meta.json", audioMetaStr);
 
-  retrieveAudioCover(audioPath, folderPath + "cover.jpg");
-  convertAudio(audioPath, folderPath + "song.ogg");
+  await convertAudio(audioPath, folderPath + "song.ogg");
   await fs.copyFile(audioPath, folderPath + "originalAudio/" + path.basename(audioPath));
-}
+
+  await fs.rm('./temp/', { recursive: true, force: true });
+};
